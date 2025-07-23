95a58b4ecf3cb4c6ac1a546059349a3e6a0edfbe
piriram <pyoram25@gmail.com>
2025-07-23 19:34:51 +0900

    Initial Commit

---
+++ NetworkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkProject
+//
+//  Created by piri kim on 7/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
