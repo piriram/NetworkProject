95527135d015aff847207015bb921cd90706acde
piriram <pyoram25@gmail.com>
2025-07-24 20:38:18 +0900

    Feat: 통신 실패시 알림창을 보여줌

--- NetworkProject/Extension/Box+Data.swift
+++ NetworkProject/Extension/Box+Data.swift
@@ -42,13 +42,16 @@ extension BoxOfficeViewController {
                     dump(value)
                 case .failure(_):
                     print("error 발생함")
+                    self.showAlert(title: "검색실패", message: "yyyyMMdd 형식에 맞춰주세요.")
                     self.movies = []
                     self.tableView.reloadData()
                 }
-                
         }
-        
-        
-//        tableView.reloadData()
     }
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        present(alert, animated: true)
+    }
+
 }
